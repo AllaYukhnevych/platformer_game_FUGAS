8c140d69fe2d08019780078e3efc49af67734060
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-01-28 12:53:22 +0200

    After reaching the finish line, allows you to move on to the next level

---
+++ Medieval Adventure/Assets/Script/Finish.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
