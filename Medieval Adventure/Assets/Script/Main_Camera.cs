c321dc33ed076a016d1b2303d3b5e4089ab4d8c3
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-02-03 13:55:52 +0200

    The camera is set up, follows the character and does not go beyond

--- Medieval Adventure/Assets/Script/Main_Camera.cs
+++ Medieval Adventure/Assets/Script/Main_Camera.cs
@@ -4,11 +4,71 @@ using UnityEngine;
 
 public class Main_Camera : MonoBehaviour
 {
-    public Transform trackingObject;
-    
-    
+    public float dumping = 1.5f;
+    public Vector2 offset = new Vector2(2f, 0f);
+    public bool isLeft;
+    private Transform player;
+    private int LastX;
+
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float upLimit;
+    [SerializeField]
+    float downLimit;
+
+    void Start()
+    {
+       offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(isLeft);
+    }
+
+    public void FindPlayer(bool playerIsLeft)
+    {
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+       LastX = Mathf.RoundToInt(player.position.x);
+
+        if (playerIsLeft)
+        {
+            transform.position = new Vector3(player.position.x - offset.x, player.position.y - offset.y, transform.position.z);
+        }
+
+        else
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+        }
+    }
+
     void Update()
     {
-        transform.position = new Vector3(trackingObject.position.x, transform.position.y, transform.position.z);
+        if (player)
+        {
+            int currentX = Mathf.RoundToInt(player.position.x);
+            if (currentX > LastX) isLeft = false; else if (currentX < LastX) isLeft = true;
+            LastX = Mathf.RoundToInt(player.position.x);
+
+            Vector3 target;
+            if (isLeft)
+            {
+                target = new Vector3(player.position.x - offset.x, player.position.y - offset.y, transform.position.z);
+            }
+            else
+            {
+                target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+            }
+
+            Vector3 currentPosition = Vector3.Lerp(transform.position, target, dumping * Time.deltaTime);
+            transform.position = currentPosition;
+        }
+
+        transform.position = new Vector3
+            (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, downLimit, upLimit),
+            transform.position.z
+            ); 
     }
 }
+
