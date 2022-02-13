d2cc823bcd8169684f9592bd693d2531bdec45da
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-02-13 23:04:03 +0200

    added sound when collecting the chest

--- Medieval Adventure/Assets/Script/Coins.cs
+++ Medieval Adventure/Assets/Script/Coins.cs
@@ -1,11 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Coins : MonoBehaviour
 {
-    
     private GameObject player;
     public AudioClip audioClip;
     private AudioSource audioSource;
