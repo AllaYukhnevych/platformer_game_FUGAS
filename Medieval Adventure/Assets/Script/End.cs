75fc0b03922a7a449cf26f9fd2aca25bbc3da68a
Alla Yukhnevych <allauhnevich25@gmail.com>
2022-02-13 23:02:24 +0200

    window added "You win"

--- Medieval Adventure/Assets/Script/End.cs
+++ Medieval Adventure/Assets/Script/End.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 
-
 public class End : MonoBehaviour
 {
     List<InputEntry> entries = new List<InputEntry>();
